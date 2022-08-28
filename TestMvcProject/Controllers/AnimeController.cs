62e1b94488d84bbd1108fe0828e645ebc7710beb
Toga <rodionphilippov@gmail.com>
2022-08-28 15:57:03 +0400

    anime controller done

--- TestMvcProject/Controllers/AnimeController.cs
+++ TestMvcProject/Controllers/AnimeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JikanDotNet;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TestMvcProject.Data;
 using TestMvcProject.Models;
 using TestMvcProject.ViewModels;
+using Anime = TestMvcProject.Models.Anime;
 
 namespace TestMvcProject.Controllers
 {
@@ -66,6 +68,7 @@ namespace TestMvcProject.Controllers
         {
             ViewBag.AuthorList = await _viewHelper.FillViewBagAuthorList(_appDbContext);
             ViewBag.MangaList = await _viewHelper.FillViewBagMangaList(_appDbContext);
+            ViewBag.GenreList = await _viewHelper.FillViewBagGenreList(_appDbContext);
             return View();
         }
 
@@ -77,21 +80,44 @@ namespace TestMvcProject.Controllers
             if (!ModelState.IsValid)
                 return View(anime);
 
+            var mangas = _appDbContext.Mangas;
+            var authors = _appDbContext.Authors;
+            var genres = _appDbContext.Genres;
+
             if (anime.Avatar != null)
             {
-                var img = _viewHelper.GetImg(anime.Avatar, "PosterOf" + anime.Tittle);
-                anime.Images.Add(img);
+                var img = _viewHelper.GetImg(anime.Avatar, "AvatarOf" + anime.Tittle);
+
+                //using (var image = new MagickImage(img.Data))
+                //{
+                //    image.Resize(300, 0);
+                //    img.Data = image.ToByteArray();
+                //}
+
+                anime.Images?.Add(img);
             }
+
+            if (anime.AuthorId != null)
+                anime.Authors?.AddRange(_appDbContext.Authors.Where(a => a.Id == anime.AuthorId));
+
+            if (anime.MangaIdList != null && anime.MangaIdList.Count > 0)
+                anime.Manga?.AddRange(mangas.Where(a => anime.MangaIdList.Any(m => m == a.Id)).ToList());
+
+            if (anime.AuthorIdList != null && anime.AuthorIdList.Count > 0)
+                anime.Authors?.AddRange(authors.Where(a => anime.AuthorIdList.Any(m => m == a.Id)).ToList());
+
+            if (anime.GenreIdList != null && anime.GenreIdList.Count > 0)
+                anime.Genres?.AddRange(genres.Where(g => anime.GenreIdList.Any(m => m == g.Id)).ToList());
 
             _appDbContext.Animies.Add(anime);
             await _appDbContext.SaveChangesAsync();
 
-            TempData["success"] = "Anime created successfully!";
+            TempData["success"] = "Manga created successfully!";
 
             return RedirectToAction("Index");
         }
 
-        // GET: PositionController/Delete/5
+        // GET: Anime/Delete/5
         public async Task<ActionResult> DeleteAsync(Guid? id)
         {
             if (id == Guid.Empty || id == null)
@@ -135,19 +161,24 @@ namespace TestMvcProject.Controllers
             return RedirectToAction("Index");
         }
 
-        // GET: PositionController/Edit/5
-        public async Task<ActionResult> Edit(Guid? id)
+        // GET: Anime/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
         {
-            if (id == Guid.Empty || id == null)
+            if (id == null || Guid.Empty == id)
                 return NotFound();
 
-            var anime = await _appDbContext.Animies.FirstOrDefaultAsync(a => a.Id == id);
+            var anime = await _appDbContext.Animies.Include(m => m.Images)
+                .Include(m => m.Authors)
+                .Include(m => m.Manga)
+                .Include(m => m.Genres)
+                .FirstOrDefaultAsync(m => m.Id == id);
 
             if (anime == null)
                 return NotFound();
 
-            ViewBag.AuthorList = await _viewHelper.FillViewBagAuthorList(_appDbContext);
             ViewBag.MangaList = await _viewHelper.FillViewBagMangaList(_appDbContext);
+            ViewBag.AuthorList = await _viewHelper.FillViewBagAuthorList(_appDbContext);
+            ViewBag.GenreList = await _viewHelper.FillViewBagGenreList(_appDbContext);
 
             return View(anime);
         }
